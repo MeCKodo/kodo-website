8134d6a9126fe49b61aa75ce858d7606b4506dac
kodo <524580860@qq.com>
2017-09-25 14:27:41 +0800

    add: banner component

---
+++ components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import * as s from './style';
+
+export default () => (
+  <s.Banner>
+    <s.Title>
+      像我这样毫无能力又没有色彩的人
+      <br/>
+      <i>也就只能在失去中成长了吧</i>
+    </s.Title>
+  </s.Banner>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import { Component } from "react";
 import styled from "styled-components";
 import Header from "../components/Header/Header";
+import Banner from "../components/Banner/Banner";
+
 
 const Title = styled.h1`
   color: red;
@@ -12,6 +14,7 @@ export default class extends Component {
     return (
       <div id="home">
         <Header />
+        <Banner />
         <Title>My page</Title>
       </div>
     );
