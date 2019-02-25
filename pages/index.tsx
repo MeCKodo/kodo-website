3a8dedca9adcf5fa8dfd2f75ee936c5d950a213e
MecKodo <524580860@qq.com>
2019-02-25 16:58:40 +0800

    test fetch data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,27 @@
+// import { NextContext } from "next";
 import { Component } from "react";
 import styled from "styled-components";
-
-import Header from "../components/Header/Header";
-import Banner from "../components/Banner/Banner";
+import axios from "axios";
+// import Header from "../components/Header/Header";
+// import Banner from "../components/Banner/Banner";
 
 const Title = styled.h1`
   color: red;
-  font-size: 50px;
+  font-size: 10px;
 `;
 
 export default class extends Component {
+  static async getInitialProps() {
+    // eslint-disable-next-line no-undef
+    const res = await axios("https://api.github.com/repos/zeit/next.js");
+    console.log(res.data);
+    return {};
+  }
   render() {
     return (
       <div id="home">
-        <Header />
-        <Banner />
+        {/* <Header /> */}
+        {/* <Banner /> */}
         <Title>My page</Title>
       </div>
     );
